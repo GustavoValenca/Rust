acafa57ec2d4134710fb9249eb4033b2a953adc1
Gustavo <gustavovalenca15@gmail.com>
2023-05-10 11:00:48 -0300

    getting started with rust

---
+++ hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d
@@ -0,0 +1,5 @@
+/home/gustavoski/CS/My Projects/Rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc: src/main.rs
+
+/home/gustavoski/CS/My Projects/Rust/hello_cargo/target/debug/deps/hello_cargo-a79e5d8829485ddc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/gustavoski/CS/My\ Projects/Rust/hello_cargo/target/debug/hello_cargo: /home/gustavoski/CS/My\ Projects/Rust/hello_cargo/src/main.rs
