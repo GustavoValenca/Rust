0e00a3ce692666077d855c41c56f6dfd14d7c716
Gustavo <gustavovalenca15@gmail.com>
2023-09-18 23:13:19 -0300

    sorting

--- Practice Problems/median_and_mode/src/main.rs
+++ Practice Problems/median_and_mode/src/main.rs
@@ -15,5 +15,38 @@ fn main() {
         }
     }
 
+    println!("Original vector: ");
     println!("{:?}", v);
+
+    let mut key : i32;
+    let mut j : usize;
+    let mut j_signed : i32;
+
+    for i in 1..13 {
+
+        key = v[i];
+        
+        j = i - 1;
+        j_signed = j as i32;
+
+        while v[j] > key && j_signed >= 0 {
+            v[j + 1] = v[j];
+            j_signed -= 1;
+            if j_signed < 0 {
+                break;
+            }
+            j -= 1;
+
+        }
+        if j_signed < 0 {
+            v[0] = key;
+        }
+        else {
+            v[j + 1] = key;
+        }
+
+        println!("After execution {i}");
+        println!("{:?}", v);
+    }
 }
+
