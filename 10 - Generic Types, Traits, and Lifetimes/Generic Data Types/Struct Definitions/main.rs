86aec67c434029329858c0e2b2a49f2795bbd756
Gustavo <gustavovalenca15@gmail.com>
2023-10-07 22:50:43 -0300

    generic data types

---
+++ 10 - Generic Types, Traits, and Lifetimes/Generic Data Types/Struct Definitions/main.rs
@@ -0,0 +1,23 @@
+// Same type
+
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// fn main(){
+//     let integer = Point { x: 5, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+// }
+
+// Same or different type
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+fn main(){
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0};
+}
