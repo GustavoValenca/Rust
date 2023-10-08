a624a57f13ac43c85191eaf2b97594154527dcd0
Gustavo <gustavovalenca15@gmail.com>
2023-10-08 15:25:05 -0300

    traits

---
+++ 10 - Generic Types, Traits, and Lifetimes/Traits/aggregator/src/lib.rs
@@ -0,0 +1,144 @@
+pub trait Summary {
+    // requires implementation on every type
+    // fn summarize(&self) -> String;  
+
+    // can be overridden
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+
+    fn summarize_author(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+// required implementation when there is no default implementation
+// impl Summary for NewsArticle {
+//     fn summarize(&self) -> String {
+//         format!("{}, by {} ({})", self.headline, self.author, self.location)
+//     }
+// }
+
+// default implementation
+// impl Summary for NewsArticle {}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+// syntax 
+pub fn notify(item: &impl Summary){  // any type that implements the trait Summary
+    println!("Breaking news! {}", item.summarize());
+}
+
+// trait bound
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// Specifying Multiple Traits
+// pub fn notify(item: &(impl Summary + Display)){
+
+// }
+
+// pub fn notify<T: Summary + Display>(item: &T){
+
+// }
+
+// Clearer Trait Bounds
+
+fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {}
+
+fn some_function<T, U>(t: &T, u: &U) -> i32 
+where 
+    T: Display + Clone,
+    U: Clone + Debug,
+{}
+
+// Returning Types that implement Traits
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people"
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
+// We can use impl trai syntax only if returning a single type.
+// The code below doesn't work
+// fn returns_summarizable(switch: bool) -> impl Summary {
+//     if switch {
+//         NewsArticle {
+//             headline: String::from(
+//                 "Penguins win the Stanley Cup Championship!",
+//             ),
+//             location: String::from("Pittsburgh, PA, USA"),
+//             author: String::from("Iceburgh"),
+//             content: String::from(
+//                 "The Pittsburgh Penguins once again are the best \
+//                  hockey team in the NHL.",
+//             ),
+//         }
+//     } else {
+//         Tweet {
+//             username: String::from("horse_ebooks"),
+//             content: String::from(
+//                 "of course, as you probably already know, people",
+//             ),
+//             reply: false,
+//             retweet: false,
+//         }
+//     }
+// }
+
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest number is x = {}", self.x);
+        }
+        else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
