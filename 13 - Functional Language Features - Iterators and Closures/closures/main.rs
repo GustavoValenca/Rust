c37bc204fe0ebec5fd598036707f9ccc07cf6f54
Gustavo <gustavovalenca15@gmail.com>
2023-10-30 19:14:56 -0300

    autômatos

--- 13 - Functional Language Features - Iterators and Closures/closures/main.rs
+++ 13 - Functional Language Features - Iterators and Closures/closures/main.rs
@@ -96,27 +96,48 @@
 //     println!("{:#?}", list);
 // }
 
-// Não compila
+// // Não compila
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main(){
+//     let mut list = [
+//         Rectangle { width: 10, height: 1},
+//         Rectangle { width: 3, height: 5},
+//         Rectangle { width: 7, height: 12},
+//     ];
+
+//     let mut sort_operations = vec![];
+
+//     let value = String::from("by key called");
+
+//     list.sort_by_key(|r| {
+//         sort_operations.push(value);
+//         r.width
+//     });
+//     println!("{:#?}", list);
+// }
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
-fn main(){
+fn main() {
     let mut list = [
-        Rectangle { width: 10, height: 1},
-        Rectangle { width: 3, height: 5},
-        Rectangle { width: 7, height: 12},
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
     ];
 
-    let mut sort_operations = vec![];
-
-    let value = String::from("by key called");
-
+    let mut num_sort_operations = 0;
     list.sort_by_key(|r| {
-        sort_operations.push(value);
+        num_sort_operations += 1;
         r.width
     });
-    println!("{:#?}", list);
+    println!("{:#?}, sorted in {num_sort_operations} operations", list);
 }
